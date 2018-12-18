234b5db5350c9fde0f41177918e0507db6a8c655
卿九 <31655306+Yueli23@users.noreply.github.com>
2018-12-18 21:12:00 +0800

    实验6

---
+++ 实验6/SixthH1/sixthH1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sixthH1
+//
+//  Created by 南希 on 2018/11/27.
+//  Copyright © 2018年 NanXi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ 实验6/SixthH2/实验六第一题/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  实验六第一题
+//
+//  Created by 南希 on 2018/12/16.
+//  Copyright © nanxi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
